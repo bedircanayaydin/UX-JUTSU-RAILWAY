c8bf5738bd22557cb9947350f4a5a3f94619fc54
bedircan ayaydın <40080969+bedircanayaydin@users.noreply.github.com>
2021-12-06 14:05:59 +0300

    Add files via upload

---
+++ userge/plugins/jutsu/langs.py
@@ -0,0 +1,9 @@
+# created for USERGE-X by @Kakashi_HTK(telegram)/@ashwinstr(github)
+
+
+def sort_lang(LANGUAGES):
+    lang_list = []
+    for code in LANGUAGES:
+        lang_list.append(f'''"{code}": "{LANGUAGES[code]}"''')
+    lang_list = "{\n    " + ",\n   ".join(lang_list) + "\n}"
+    return lang_list
